009e4862218c6dd2d346565ef1106784193d2df4
lymlym123123 <57713623+lymlym123123@users.noreply.github.com>
2021-03-02 10:08:35 +0800

    Update and rename export_parent/export_system_service/src/main/java/cn/itcast/service/company/userservice.java to export_parent/export_system_service/src/主要的/爪哇/cn/播客/服务/公司/userservice.java

--- export_parent/export_system_service/src/main/java/cn/itcast/service/company/userservice.java
+++
@@ -1,4 +0,0 @@
-package cn.itcast.service.company;
-
-public class userservice {
-}
---
+++ export_parent/export_system_service/src/主要的/爪哇/cn/播客/服务/公司/userservice.java
@@ -0,0 +1,7 @@
+package cn.itcast.service.company;
+
+public class userservice {
+    public static void main(String[] args) {
+        int i=2;
+    }
+}
